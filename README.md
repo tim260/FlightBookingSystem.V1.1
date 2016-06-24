e2e7b4492936ca86f21b1fe32451f56d0b9b8600
Parviz Khorrami <pkhorraminejad@ymail.com>
2016-06-24 13:36:23 +0430

    Updates

--- README.md
+++ README.md
@@ -10,6 +10,8 @@ Install:
     3.Copy the following to core/local.config.php :--Config::Set('JUMPSEAT_COST', '.25'); //0.25 is the cost.
     4.Access it by <?php echo url('/FBSV11") ;?>
     5.Enjoy.
+
+Note: If you have installed FuelCalculator.V1.2, there are extra codes that you can add to schedule_results.php to show fuel calculations. Copy the content of extra.codes.tpl and paste it in the specified place.
 ---------------------------------------------------------
 Support:
 
--- core/templates/fbsv/schedule_results.php
+++ core/templates/fbsv/schedule_results.php
@@ -9,20 +9,17 @@ $last_name = OperationsData::getAirportInfo($last_location->arricao);
 	<li>Available flights from: <b><font color="#FF3300"><?php echo $last_location->arricao.' ( '.$last_name->name.')' ;?></font></b></li>
 </ul>
 <table>
-<?php
-if(!$allroutes)
-{
-?>
-	<tr><td align="center">No flights from <?php echo $last_location->arricao.' ( '.$last_name->name.')' ;?>!</td></tr>
-	
-<?php
-}
-else
-{
-?>
-
-
-<thead>
+	<?php
+	if(!$allroutes)
+	{
+	?>
+		<tr><td align="center">No flights from <?php echo $last_location->arricao.' ( '.$last_name->name.')' ;?>!</td></tr>
+		
+	<?php
+	}
+	else
+	{
+	?>
 <tr>
     <th>Flight ID</th>
     <th>Origin</th>
@@ -30,25 +27,23 @@ else
     <th>Aircraft</th>
     <th>Options</th>
 </tr>
-</thead>
-<?php
-foreach($allroutes as $route)
-{
-	if($last_location->arricao != $route->depicao)
+	<?php
+	foreach($allroutes as $route)
 	{
-		continue;
-	}
-	if(Config::Get('RESTRICT_AIRCRAFT_RANKS') === true && Auth::LoggedIn())
-	{
-		$s="SELECT * FROM phpvms_aircraft WHERE name='$route->aircraft'";
-		$ss=DB::get_row($s);
-		if($ss->minrank > Auth::$userinfo->ranklevel)
+		if($last_location->arricao != $route->depicao)
 		{
 			continue;
 		}
-	}
-?>
-
+		if(Config::Get('RESTRICT_AIRCRAFT_RANKS') === true && Auth::LoggedIn())
+		{
+			$s="SELECT * FROM phpvms_aircraft WHERE name='$route->aircraft'";
+			$ss=DB::get_row($s);
+			if($ss->minrank > Auth::$userinfo->ranklevel)
+			{
+				continue;
+			}
+		}
+	?>
 <tr>
 	<td><?php echo $route->code . $route->flightnum?></td>
 	<td><?php echo $route->depicao ;?></td>
@@ -80,155 +75,128 @@ foreach($allroutes as $route)
         ?>
     </td>
 </tr>
-
-        <td colspan="5">
-		
+<tr>
+    <td colspan="5">
 		<table id="details_dialog_<?php echo $route->flightnum;?>" style="display:none">
-			<thead>
 			<tr>
-			<th colspan="4">&nbsp;</th>
-			</tr>
-			<tr>
-			<th style="text-align: center;" bgcolor="#00405e" colspan="4"><font color="white">Flight Brefing</font></th>
-			</tr>
-			
-			<tr>
-			<td align="left">Deaprture:</td>
-			<td colspan="0" align="left" ><b><?php
-			$name = OperationsData::getAirportInfo($route->depicao);
-			echo "{$name->name}"?></b></td>
-			<td align="left">Arrival:</td>
-			<td colspan="0" align="left" ><b><?php 
-			$name = OperationsData::getAirportInfo($route->arricao);
-			echo "{$name->name}"?></b></td>
+				<th colspan="4">&nbsp;</th>
 			</tr>
 			<tr>
-			<td align="left">Aircraft</td>
-			<td colspan="0" align="left" ><b><?php 
-			$plane = OperationsData::getAircraftByName($route->aircraft);
-			echo $plane->fullname ; ?></b></td>
-			<td align="left">Distance:</td>
-			<td colspan="0" align="left" ><b><?php echo $route->distance . Config::Get('UNITS') ;?></b></td>
+				<th style="text-align: center;" bgcolor="#00405e" colspan="4"><font color="white">Flight Brefing</font></th>
 			</tr>
 			<tr>
-			<td align="left">Dep Time:</td>
-			<td colspan="0" align="left" ><b><font color="red"><?php echo $route->deptime?> GMT</font></b></td>
-			<td align="left">Arr Time:</td>
-			<td colspan="0" align="left" ><b><font color="red"><?php echo $route->arrtime?> GMT</font></b></td>
+				<td align="left">Deaprture:</td>
+				<td colspan="0" align="left" >
+					<b>
+					<?php
+					$name = OperationsData::getAirportInfo($route->depicao);
+					echo $name->name;
+					?>
+					</b>
+				</td>
+				<td align="left">Arrival:</td>
+				<td colspan="0" align="left" >
+					<b>
+					<?php 
+					$name = OperationsData::getAirportInfo($route->arricao);
+					echo $name->name;
+					?>
+					</b>
+				</td>
 			</tr>
 			<tr>
-			<td align="left">Altitude:</td>
-			<td colspan="0" align="left" ><b><?php echo $route->flightlevel; ?> ft</b></td>
-			<td align="left">Duration:</td>
-			<td colspan="0" align="left" ><font color="red"><b>
-			<?php 
-			
-			$dist = $route->distance;
-			$speed = 440;
-			$app = $speed / 60 ;
-			$flttime = round($dist / $app,0)+ 20;
-			$hours = intval($flttime / 60);
-            $minutes = (($flttime / 60) - $hours) * 60;
-			if($hours > "9" AND $minutes > "9")
-			{
-			echo $hours.':'.$minutes ;
-			}
-			else
-			{
-			echo '0'.$hours.':'.$minutes ;
-			}
-			?> Hrs</b></font></td>
+				<td align="left">Aircraft</td>
+				<td colspan="0" align="left" >
+					<b>
+					<?php 
+					$plane = OperationsData::getAircraftByName($route->aircraft);
+					echo $plane->fullname ; 
+					?>
+					</b>
+				</td>
+				<td align="left">Distance:</td>
+				<td colspan="0" align="left" ><b><?phpecho $route->distance . Config::Get('UNITS') ;?></b></td>
 			</tr>
 			<tr>
-			<td align="left">Days</td>
-			<td colspan="0" align="left" ><b><?php echo Util::GetDaysLong($route->daysofweek) ;?></b></td>
-			<td align="left">Price:</td>
-			<td colspan="0" align="left" ><b><?php echo $route->price ;?>.00</b></td>
+				<td align="left">Dep Time:</td>
+				<td colspan="0" align="left" ><b><font color="red"><?php echo $route->deptime?> GMT</font></b></td>
+				<td align="left">Arr Time:</td>
+				<td colspan="0" align="left" ><b><font color="red"><?php echo $route->arrtime?> GMT</font></b></td>
 			</tr>
 			<tr>
-			<td align="left">Flight Type:</td>
-			<td colspan="0" align="left" ><b><?php
-			if($route->flighttype == "P")
-			{
-			echo'Passenger' ;
-			}
-			if($route->flighttype == "C")
-			{
-			echo'Cargo' ;
-			}
-			if($route->flighttype == "H")
-			{
-			echo'Charter' ;
-			}
-			?></b></td>
-			<td align="left">Flown</td>
-			<td colspan="0" align="left" ><b><?php echo $route->timesflown ;?></b></td>
+				<td align="left">Altitude:</td>
+				<td colspan="0" align="left" ><b><?php echo $route->flightlevel; ?> ft</b></td>
+				<td align="left">Duration:</td>
+				<td colspan="0" align="left" >
+					<font color="red">
+					<b>
+					<?php 
+					
+					$dist = $route->distance;
+					$speed = 440;
+					$app = $speed / 60 ;
+					$flttime = round($dist / $app,0)+ 20;
+					$hours = intval($flttime / 60);
+					$minutes = (($flttime / 60) - $hours) * 60;
+					if($hours > "9" AND $minutes > "9")
+					{
+					echo $hours.':'.$minutes ;
+					}
+					else
+					{
+					echo '0'.$hours.':'.$minutes ;
+					}
+					?> Hrs
+					</b>
+					</font>
+				</td>
 			</tr>
-			<tr><td>Route:</td><td colspan="3" align="left" ><b><?php echo $route->route ;?></b></td>
-			</tr>
-			
 			<tr>
-			<th style="text-align: center;" bgcolor="#00405e" colspan="4"><font color="white">Fuel Calculation for <?php 
-			$plane = OperationsData::getAircraftByName($route->aircraft);
-			echo $plane->fullname ; ?>
-			</font></th>
+				<td align="left">Days</td>
+				<td colspan="0" align="left" ><b><?php echo Util::GetDaysLong($route->daysofweek) ;?></b></td>
+				<td align="left">Price:</td>
+				<td colspan="0" align="left" ><b><?php echo $route->price ;?>.00</b></td>
 			</tr>
-			<?php
-			$aircraft = $plane->name;
-			$distance = $route->distance;
-			$param = FCalculator::getparamaircraft($aircraft);
-			$fuel_hour = $param->hour;
-			$fuel_reserve = $param->hour * 3/4;
-			$fuel_flow = $param->flow;
-			$speed = $param->speed;
-			$fuel_taxi = 200;
-            $total = FCalculator::calculatefuel($aircraft, $distance);      
-			?> 	
-						 <tr>
-			             <td align="left" colspan="2">Average Cruise Speed:</td>
-						 <td align="left" colspan="2"><b><?php echo $speed;?> kt/h - 800 km/h</b></td>
-						 </tr>
-						 <tr>
-			             <td align="left" colspan="2">Fuel Per 1 Hour:</td>
-						 <td align="left" colspan="2"><b><?php	echo $fuel_hour ;?> kg - <?php echo ($fuel_hour *2.2) ;?> lbs</b></td>
-						 </tr>
-						 <tr>
-						 <td align="left" colspan="2">Fuel Per 100 NM:</td>
-						 <td align="left" colspan="2"><b><?php echo $fuel_flow ;?> kg - <?php echo ($fuel_flow *2.2) ;?> lbs</b></td>
-						 </tr>
-						 <tr>
-						 <td align="left" colspan="2">Taxi Fuel:</td>
-						 <td align="left" colspan="2"><b><?php echo $fuel_taxi ;?> kg - <?php echo ($fuel_taxi *2.2) ;?> lbs</b></td>
-						 <tr>
-						 <td align="left" colspan="2">Minimum Fuel Requiered At Destination:</td>
-						 <td align="left" colspan="2"><b><?php echo $fuel_reserve ;?> kg - <?php echo ($fuel_reserve *2.2) ;?> lbs</b></td>
-						 </tr>
-						 <tr>
-						 <td align="center" colspan="4"><font color="blue" size="4">Total Estimated Fuel Requiered For This Route:&nbsp;&nbsp;&nbsp;<?php echo round($total, 1) ;?> kg - <?php echo round(($total *2.2), 1) ;?> lbs</font></td>
-						 </tr>
-                         <tr>
-						 <td align="center" colspan="4"><font size="3" color="red"><b>TO PREVENT ANY MISCALCULATION ADD 500 KG EXTRA!</b></font></td>                                  
-                         </tr>
-			</td>
-			</tr>
-			
 			<tr>
-			<th style="text-align: center;" bgcolor="#00405e" colspan="4"><font color="white">Flight Map</font></th>
+				<td align="left">Flight Type:</td>
+				<td colspan="0" align="left" >
+					<b>
+					<?php
+					if($route->flighttype == "P")
+					{
+					echo'Passenger' ;
+					}
+					if($route->flighttype == "C")
+					{
+					echo'Cargo' ;
+					}
+					if($route->flighttype == "H")
+					{
+					echo'Charter' ;
+					}
+					?>
+					</b>
+				</td>
+				<td align="left">Flown</td>
+				<td colspan="0" align="left" ><b><?php echo $route->timesflown ;?></b></td>
 			</tr>
-			
+			<tr><td>Route:</td><td colspan="3" align="left" ><b><?php echo $route->route ;?></b></td></tr>
 			<tr>
-			<td width="100%" colspan="4">
-			<?php
-			$string = "";
-                        $string = $string.$route->depicao.'+-+'.$route->arricao.',+';
-                        ?>
+				<th style="text-align: center;" bgcolor="#00405e" colspan="4"><font color="white">Flight Map</font></th>
+			</tr>
+			<tr>
+				<td width="100%" colspan="4">
+					<?php
+					$string = "";
+								$string = $string.$route->depicao.'+-+'.$route->arricao.',+';
+								?>
 
-                        <img width="100%" src="http://www.gcmap.com/map?P=<?php echo $string ?>&amp;MS=bm&amp;MR=240&amp;MX=680x200&amp;PM=pemr:diamond7:red%2b%22%25I%22:red&amp;PC=%230000ff" />
+								<img width="100%" src="http://www.gcmap.com/map?P=<?php echo $string ?>&amp;MS=bm&amp;MR=240&amp;MX=680x200&amp;PM=pemr:diamond7:red%2b%22%25I%22:red&amp;PC=%230000ff" />
+				</td>
 			</tr>
-			</td>
-			</thead>
-		 </table>	
-        </td>
+			//<-----------add extra codes for fuel calculations here--------------->
+		</table>
+    </td>
 </tr>
 			
 <?php
